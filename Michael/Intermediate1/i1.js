aa77a51133f02b2684f084f98adc87d3b3d3c60c
Michael Zechner <michael-zechner@gmx.at>
2021-09-27 11:26:45 +0200

    intermediate 1 draft

---
+++ Michael/Intermediate1/i1.js
@@ -0,0 +1,26 @@
+var names=["Martin","Thomas","Klaus", "Maria","David"];
+var points=[76,85,65,93,81];
+grade=(i)=>{
+    let p=points[i];
+    switch (true) {
+        case (p<60):
+            document.write(names[i]+" has an F <br>");
+            break;
+        case ((60<=p)&&(p<70)):
+            document.write(names[i]+" has a D <br>");
+            break;
+        case((70<=p)&&(p<80)):
+            document.write(names[i]+" has a C <br>");
+            break;
+        case ((80<=p)&&(p<90)):
+            document.write(names[i]+" has a B <br>");
+            break;
+        default:
+            document.write(names[i]+" has an A <br>");
+            break;
+    }
+}
+for (let i = 0; i < names.length; i++) {
+    grade(i);
+    
+}
