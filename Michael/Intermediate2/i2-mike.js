3130ad3913f03352e965f3a13305b3197fce9d7f
Michael Zechner <michael-zechner@gmx.at>
2021-09-27 13:44:05 +0200

    intermediate 2 solved

---
+++ Michael/Intermediate2/i2-mike.js
@@ -0,0 +1,16 @@
+for (let j = 0; j < 100; j++) {
+    switch (true) {
+        case ((j+1)%15===0):
+            document.write("FizzBuzz <br>");
+            break;
+        case ((j+1)%3===0):
+            document.write("Fizz <br>");
+            break;
+        case ((j+1)%5===0):
+            document.write("Buzz <br>");
+        default:
+            document.write((j+1)+"<br>");
+            break;
+    }
+    
+}
